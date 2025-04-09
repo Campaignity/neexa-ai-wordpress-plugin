8b3069e412c7f9dc2334665a9873b9e9633231f5
Simon <slemonla13@gmail.com>
2025-04-09 22:54:18 +0800

    [simon]chore: fix error

--- admin/partials/neexa-ai-user-onboarding.php
+++ admin/partials/neexa-ai-user-onboarding.php
@@ -3,5 +3,5 @@ global $neexa_ai_config;
 ?>
 
 <div id="neexa-ai-onboarding-iframe-container">
-    <iframe src="<?= $neexa_ai_config["frontend-url"] ?>/#/get-started/wordpress" frameborder="0" class="full-page-iframe"></iframe>
+    <iframe src="<?= $neexa_ai_config["frontend-host"] ?>/#/get-started/wordpress" frameborder="0" class="full-page-iframe"></iframe>
 </div>
