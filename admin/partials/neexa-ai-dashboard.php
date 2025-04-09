c6c2c768bb8cf50982ba9f5a49385960fe5284e9
Simon <slemonla13@gmail.com>
2025-04-09 23:09:38 +0800

    [simon]chore: restore

--- admin/partials/neexa-ai-dashboard.php
+++ admin/partials/neexa-ai-dashboard.php
@@ -20,7 +20,7 @@
 
 $isAuthenticated = false;
 
-if (1 || get_option('neexa_ai_access_token') && get_option('neexa_ai_access_token') != '') {
+if (get_option('neexa_ai_access_token') && get_option('neexa_ai_access_token') != '') {
 
     $accessToken = get_option('neexa_ai_access_token');
 
@@ -32,11 +32,11 @@ if (1 || get_option('neexa_ai_access_token') && get_option('neexa_ai_access_toke
 }
 
 if (!$isAuthenticated) {
-    // $getStartedExplainer = "manage, monitor";
+    $getStartedExplainer = "manage, monitor";
 
-    // require_once plugin_dir_path(__FILE__) . 'neexa-ai-get-started.php';
+    require_once plugin_dir_path(__FILE__) . 'neexa-ai-get-started.php';
 
-    // exit();
+    exit();
 }
 ?>
 
