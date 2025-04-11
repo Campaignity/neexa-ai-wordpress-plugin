be7ca4f917fd02697598db672d97aaf16428aeb7
Simon <slemonla13@gmail.com>
2025-04-11 18:18:16 +0800

    [simon]fix:  better comment documentation

--- admin/class-neexa-ai-admin.php
+++ admin/class-neexa-ai-admin.php
@@ -13,8 +13,8 @@
 /**
  * The admin-specific functionality of the plugin.
  *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
+ * Defines the plugin name, version, and enqueuing the 
+ * admin-specific stylesheet and JavaScript and other needed items
  *
  * @package    Neexa_Ai
  * @subpackage Neexa_Ai/admin
