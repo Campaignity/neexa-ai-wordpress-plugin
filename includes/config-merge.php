87f312b65135fd22142bd40cd35aee846c944e68
Simon <slemonla13@gmail.com>
2025-04-14 20:35:18 +0800

    [simon]chore: other configd

--- includes/config-merge.php
+++ includes/config-merge.php
@@ -1,7 +1,11 @@
 <?php
 
 $prod_configs = require_once(__DIR__."/config-prod.php");
-$other_configs = @include (__DIR__."/config.php") ?: [];
+$other_configs = @include (__DIR__."/config.php");
+
+if(empty($other_configs)){
+    $other_configs = [];
+}
 
 return array_merge(
     $prod_configs,
