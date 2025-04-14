4f8ca145d0955b9778b8338aaad470e5d460d4aa
Simon <slemonla13@gmail.com>
2025-04-14 20:15:55 +0800

    [simon]chore: the configs

--- includes/config-example.php
+++ includes/config-example.php
@@ -2,7 +2,7 @@
 // copy this and rename to config.php to access configs globally
 return [
     'plugin-configuration-url' => admin_url('admin.php?page=neexa-ai-configuration'),
-    'widget-loader-script-url'=>'https://chat-widget.neexa.ai/main.js',
+    'widget-loader-script-url'=>'https://chat-widget.neexa.ai/main.js',    
     'plugin-home-url' => admin_url('admin.php?page=neexa-ai-home'),    
     'frontend-host' => 'https://localhost:8089',
     'api-host' => 'http://localhost:8000/api/',
