f31661059045d086de0368bd3c54e2e41059b9af
Simon <slemonla13@gmail.com>
2025-04-14 20:26:06 +0800

    [simon]chore: proper path

--- includes/config-merge.php
+++ includes/config-merge.php
@@ -1,7 +1,7 @@
 <?php
 
-$prod_configs = require_once("./config-prod.php");
-$other_configs = @include ("./config.php") ?: [];
+$prod_configs = require_once(__DIR__."/config-prod.php");
+$other_configs = @include (__DIR__."/config.php") ?: [];
 
 return array_merge(
     $prod_configs,
