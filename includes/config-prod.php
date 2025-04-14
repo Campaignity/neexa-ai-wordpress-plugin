7d621f0e2ee18fb655a534dd0ec442038e537025
Simon <slemonla13@gmail.com>
2025-04-14 20:29:26 +0800

    [simon]chore: proper path

--- includes/config-prod.php
+++ includes/config-prod.php
@@ -1,10 +1,27 @@
 <?php
 return [
+    // 'plugin-configuration-url' => admin_url('admin.php?page=neexa-ai-configuration'),
+    // 'widget-loader-script-url'=>'https://chat-widget.neexa.ai/main.js',   
+    // 'plugin-home-url' => admin_url('admin.php?page=neexa-ai-home'),    
+    // 'frontend-host' => 'https://app.neexa.ai',
+    // 'api-host' => 'https://bkd-v2.neexa.ai/api',
+    // 'ajax-url' => admin_url('admin-ajax.php'),
+    // 'default-settings' => array(
+    //     'chat_position'   => 'bottom_right',
+    //     'appearance_mode' => 'light',
+    //     'live_status' => '1',
+    // ),
+    // 'ai-agent-roles-full-name' => array(
+    //     'salesman' => "Sales Assistant",
+    //     "qa-agent" => "Inquiry Assistant"
+    // ),
+
+
     'plugin-configuration-url' => admin_url('admin.php?page=neexa-ai-configuration'),
-    'widget-loader-script-url'=>'https://chat-widget.neexa.ai/main.js',   
+    'widget-loader-script-url'=>'https://chat-widget.neexa.ai/dev/main.js',    
     'plugin-home-url' => admin_url('admin.php?page=neexa-ai-home'),    
-    'frontend-host' => 'https://app.neexa.ai',
-    'api-host' => 'https://bkd-v2.neexa.ai/api',
+    'frontend-host' => 'https://staging.neexa.ai',
+    'api-host' => 'https://dev-bkd.neexa.ai/api',
     'ajax-url' => admin_url('admin-ajax.php'),
     'default-settings' => array(
         'chat_position'   => 'bottom_right',
