b9e01aff13194c9c96e9314977552c7e17232dac
Simon <slemonla13@gmail.com>
2025-05-02 16:02:14 +0800

    [simon]fix: bug on unused filter

--- includes/class-neexa-ai.php
+++ includes/class-neexa-ai.php
@@ -180,7 +180,6 @@ class Neexa_Ai
 		$this->loader->add_action('wp_ajax_save_neexa_ai_access_token', $plugin_admin, 'save_access_token');
 		$this->loader->add_action('wp_ajax_neexa_fetch_agents', $plugin_admin, 'fetch_ai_agents');
 		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
-		$this->loader->add_action("admin_footer", $plugin_admin, 'render_deactivate_modal');
 		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
 		$this->loader->add_action("admin_notices", $plugin_admin, 'neexa_admin_notices');
 		$this->loader->add_action('admin_menu', $plugin_admin, 'add_plugin_admin_menu');
