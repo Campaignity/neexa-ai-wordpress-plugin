00ca32a7f7de2bad1f446e397ab31fa1583db7a3
Simon <slemonla13@gmail.com>
2025-05-02 16:43:17 +0800

    [simon]chore: add to svn on copy new files

--- cp-to-svn.sh
+++ cp-to-svn.sh
@@ -4,5 +4,5 @@ if [ -z "$tag" ] || [ "$tag" = "0" ]; then
     echo "Tag is empty or zero"
 else
     cp -r ./* ./../svn/trunk
-    cp -r ./* ./../svn/tags/$tag
+    cp -r ./* ./../svn/tags/$tag && svn add --force ./../svn
 fi
