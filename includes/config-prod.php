fd682bc6a28a54e871cafdc4347d6792f8567e20
Simon <slemonla13@gmail.com>
2025-05-02 16:10:06 +0800

    [simon]fix: add trailing slash on api host

--- includes/config-prod.php
+++ includes/config-prod.php
@@ -4,8 +4,8 @@ return [
     'widget-loader-script-url'=>'https://chat-widget.neexa.ai/main.js',   
     'plugin-home-url' => admin_url('admin.php?page=neexa-ai-home'),   
     'guest_token' => 'e3f3cba00dc97a6b231ed92434b0e8dbe38d', 
-    'frontend-host' => 'https://app.neexa.ai',
-    'api-host' => 'https://bkd-v2.neexa.ai/api',
+    'frontend-host' => 'https://app.neexa.co',
+    'api-host' => 'https://bkd-v2.neexa.ai/api/',
     'ajax-url' => admin_url('admin-ajax.php'),
     'default-settings' => array(
         'chat_position'   => 'bottom_right',
